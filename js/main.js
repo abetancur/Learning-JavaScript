3f51942eda1ffa3d8bc0d69b13aa3e8963b2d458
alejandro <alejo@hotmail>
2017-07-11 14:14:16 -0500

    Instance vs Prototype V2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 "use strict";
 
 //A property "prototype is an objects" that exits on every function, but in a object it is undefined
-//Object Prototype
+//Function Prototype
 function Cat(name, color) {
     this.name = name;
     this.color = color;
 }
 Cat.prototype.age = 3;
 
-//Instance
+//Object prototype
 var fluffy = new Cat("Fluffy", "red");
 fluffy.age = 5;
 
-console.log(Cat.prototype);
-console.log(fluffy.__proto__);
+console.log(Cat.prototype.age);
+console.log(fluffy.__proto__.age);
 console.log(fluffy.hasOwnProperty("age"));
+console.log(fluffy.age);
