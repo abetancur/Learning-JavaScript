c380348517ca2d2a6e6c4a4e5c90f4094d5b7f67
alejandro <alejo@hotmail>
2017-07-11 09:08:01 -0500

    Using the Enumerable Attribute

--- js/main.js
+++ js/main.js
@@ -5,22 +5,17 @@ var cat = {
     color: "coffee"
 };
 
-//read-only properties
-console.log(Object.defineProperty(cat, "name", {writable: false}));
-//Prevent the object changed using
-Object.freeze(cat.name);
-cat.name.first = "Lucky"; //it cant not change the object read only
+//Enumerable affects see in the objects keys
+Object.defineProperty(cat, "name", {enumerable: true});
 
-//Show property descriptor
-console.log(Object.getOwnPropertyDescriptor(cat, "name"));
+for (var propertyName in cat) {
+    console.log(propertyName + ":" + cat[propertyName]);
+}
 
-//read-only properties
-/*
-console.log(Object.defineProperties(cat, {
-    "name": {
-        writable: false
-    }
-}));
-//Show property descriptor
-console.log(Object.getOwnPropertyDescriptors(cat, "name"));*/
+console.log(Object.keys(cat));
 
+//Enumerable affects JSON of the object
+console.log(JSON.stringify(cat));
+
+//We can see property
+console.log(cat["name"]);
