c743540c71924d3f6d72abcb471b80c178a2e8b7
alejandro <alejo@hotmail>
2017-07-11 09:34:30 -0500

    Using the Configurable Attribute

--- js/main.js
+++ js/main.js
@@ -5,17 +5,6 @@ var cat = {
     color: "coffee"
 };
 
-//Enumerable affects see in the objects keys
-Object.defineProperty(cat, "name", {enumerable: true});
-
-for (var propertyName in cat) {
-    console.log(propertyName + ":" + cat[propertyName]);
-}
-
-console.log(Object.keys(cat));
-
-//Enumerable affects JSON of the object
-console.log(JSON.stringify(cat));
-
-//We can see property
-console.log(cat["name"]);
+Object.defineProperty(cat, "name", {configurable: false});
+Object.defineProperties(cat, {configurable: false});
+Object.defineProperty(cat, "color", {enumerable: false});
