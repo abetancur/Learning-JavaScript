acfb211480cb915b02626c634d12f936acb8c7a0
alejandro <alejo@hotmail>
2017-07-12 19:54:02 -0500

    Creating prototype with Class

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
 "use strict";
 
-//A property "prototype is an objects" that exits on every function, but in a object it is undefined
-//Function Prototype
-function Cat(name, color) {
-    this.name = name;
-    this.color = color;
+class Animal {
+    constructor(voice) {
+        this.voice = voice || "grunt";
+    }
+
+    speak() {
+        console.log(this.voice);
+    }
 }
-Cat.prototype.age = 3;
 
-//Object prototype
-var fluffy = new Cat("Fluffy", "red");
-fluffy.age = 5;
+class Cat extends Animal {
+    constructor(name, color) {
+        super("Meow");
+        this.name = name;
+        this.color = color;
+    }
+}
 
-console.log(Cat.prototype.age);
-console.log(fluffy.__proto__.age);
-console.log(fluffy.hasOwnProperty("age"));
-console.log(fluffy.age);
+var fluffy = new Cat("fluffy", "coffee");
+console.log(fluffy);
+console.log(fluffy.constructor);
+console.log(fluffy.__proto__.__proto__);
